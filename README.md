006add1c2cb12e73bec237d5a87a499fb5fe7dc1
marwanhalfaya <marwanhalfaya@gmail.com>
2021-10-24 02:25:22 +0200

    last update

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Landing Page Project
 
+during the landing page project we applied a handful of concepts that we leared 
+we built a navigation bar to take us from a place to the other in our page
+our activty in each section is shown when we scroll by it 
+when we click the nav bar it scrolls smoothly to the section of the paragraph
+
 ## Table of Contents
 
 * [Instructions](#instructions)
@@ -11,3 +16,4 @@ The starter project has some HTML and CSS styling to display a static version of
 To get started, open `js/app.js` and start building out the app's functionality
 
 For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
--- js/app.js
+++ js/app.js
@@ -1,18 +1,3 @@
-(function(){
-    "use strict"
-
-
-    window.addEventListener("scroll", function(){
-        const navbar__list =document.querySelector(".page__header");
-        navbar__list.classList.toggle("sticky",window.scrollY>0);
-      })
-
-    
-    
-   
-
-
-
 /**
  * 
  * Manipulating the DOM exercise.
@@ -38,13 +23,29 @@
  * 
 */
 
+// counter to specify attributes and number of section
+let counter = 0;
+const createSection = () => {
+  counter++;
+  const navBar = `<section id="section${counter}" data-nav="Section ${counter}">`;
+};
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+/**
+ * make list items equal to the number of sections by iterate on them*/
 
+ const navBar = document.getElementById("navbar__list");
+ const createNavItems = () => {
+   navBar.innerHTML = "";
+   document.querySelectorAll("section").forEach((section) => {
+     const listItem = `<li><a href="#${section.id}" data-nav="${section.id}" class="menu__link">${section.dataset.nav}</a></li>`;
+     navBar.insertAdjacentHTML("beforeend", listItem);
+   });
+ };
 
 
 /**
@@ -52,6 +53,45 @@
  * Begin Main Functions
  * 
 */
+
+// using Element.getBoundingClientRect() to provide information about the size of the element and its position relative to its view point 
+window.onscroll = function() {
+	document.querySelectorAll("section").forEach(function(active) {
+    let activeLink = navBar.querySelector(`[data-nav=${active.id}]`);
+	if(active.getBoundingClientRect().top >= -400 && active.getBoundingClientRect().top <= 150){
+
+    active.classList.add("your-active-class");
+    activeLink.classList.add("active-link");
+
+    }
+    else{
+         active.classList.remove("your-active-class");
+         activeLink.classList.remove("active-link");
+    }
+	});
+}
+
+
+//on clicink the the icon it scrolls smoothly 
+navBar.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.dataset.nav) {
+    document
+      .getElementById(`${event.target.dataset.nav}`)
+      .scrollIntoView({ behavior: "smooth" });
+    setTimeout(() => {
+      location.hash = `${event.target.dataset.nav}`;
+    }, 200);
+  }
+});
+
+/**
+ * create four-section dynamically by javascript instead of HTML
+ * create them links
+ * ability to observe sections
+ */
+for (let i = 1; i < 5; i++) createSection();
+createNavItems();
 
 // build the nav
 
@@ -75,4 +115,10 @@
 // Set sections as active
 
 
-})();
+
+
+
+
+
+
+
